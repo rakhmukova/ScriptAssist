47785f657aa87f16962cf307eb15c6a996778cb6
rakhmukova <lessness19@gmail.com>
2023-03-23 16:17:36 +0300

    Add stylesheet file

--- src/main.py
+++ src/main.py
@@ -5,5 +5,9 @@ from src.ui.mainwindow import MainWindow
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
+
+    with open("resources/styles.qss", "r") as file:
+        app.setStyleSheet(file.read())
+
     window.show()
     app.exec()
