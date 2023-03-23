65721882da1a4e2aa7b1348238f473c4c77ed031
rakhmukova <lessness19@gmail.com>
2023-03-23 22:07:44 +0300

    Add action to open a dialog with configuration choise

--- src/ui/mainwindow.py
+++ src/ui/mainwindow.py
@@ -1,6 +1,7 @@
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QTextEdit, QPlainTextEdit
 
+from src.ui.dialogues.script_config_dialog import ScriptConfigDialog
 from src.ui.widgets.editor_pane import EditorPane
 from src.ui.widgets.output_pane import OutputPane
 
@@ -27,3 +28,15 @@ class MainWindow(QMainWindow):
         self.run_action.setShortcut('Ctrl+R')
         self.run_action.triggered.connect(self.output_pane.run_script)
         self.addAction(self.run_action)
+
+        self.edit_config_action = QAction('Edit configuration', self)
+        self.edit_config_action.setShortcut('Ctrl+E')
+        self.edit_config_action.triggered.connect(self.edit_script_config)
+        self.addAction(self.edit_config_action)
+
+        self.current_config = None
+
+    def edit_script_config(self):
+        config_dialog = ScriptConfigDialog()
+        config_dialog.exec()
+        self.current_config = config_dialog.get_script_config()
