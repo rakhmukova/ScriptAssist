3cd13ac5e212d179dfab3661195df4240087901f
rakhmukova <lessness19@gmail.com>
2023-03-25 19:04:33 +0300

    Highlight keywords in editor pane

--- src/ui/widgets/editor_pane.py
+++ src/ui/widgets/editor_pane.py
@@ -1,10 +1,16 @@
+from PyQt6.QtGui import QColor
 from PyQt6.QtWidgets import QPlainTextEdit
+
+from src.models.keywords_chooser import KeywordChooser
+from src.ui.widgets.keyword_highlighter import KeywordsHighlighter
 
 
 class EditorPane(QPlainTextEdit):
     def __init__(self, start_config, parent=None):
         super().__init__(parent)
         self.current_config = None
+        self.highlighter = None
+        self.keyword_color = QColor(0, 0, 255)  # blue color
         self.setObjectName('editorPane')
 
         self.set_script_config(start_config)
@@ -12,6 +18,8 @@ class EditorPane(QPlainTextEdit):
     def set_script_config(self, script_config):
         self.current_config = script_config
         self.upload_from_config()
+        keywords = KeywordChooser.get_keywords(self.current_config.script_type)
+        self.highlighter = KeywordsHighlighter(keywords, self.keyword_color, self.document())
 
     def upload_from_config(self):
         self.clear()
