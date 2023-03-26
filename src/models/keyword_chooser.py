850e792a082608c935a62feba9e0d9a6012b86d4
rakhmukova <lessness19@gmail.com>
2023-03-27 00:51:32 +0300

    Extend the list of keywords in both languages

--- src/models/keyword_chooser.py
+++ src/models/keyword_chooser.py
@@ -2,10 +2,12 @@ class KeywordChooser:
     @staticmethod
     def get_keywords(script_type):
         if script_type == 'Swift':
-            return ['break', 'case', 'continue', 'default', 'do', 'else',
-                    'fallthrough', 'if', 'return', 'switch']
+            return ['let', 'var', 'if', 'in', 'else', 'switch', 'case', 'default',
+                    'for', 'while', 'continue', 'break', 'return', 'func', 
+                    'class', 'struct', 'enum', 'extension', 'protocol', 'init', 'deinit']
         elif script_type == 'Kotlin':
-            return ['as', 'break', 'class', 'continue',
-                    'do', 'else', 'for', 'fun', 'if', 'in']
+            return ['package', 'import', 'class', 'interface', 'fun', 'val',
+                    'var', 'if', 'else', 'when', 'is', 'in', 'for', 'while',
+                    'do', 'return', 'break', 'continue', 'throw', 'try', 'catch', 'finally']
         else:
             return []
