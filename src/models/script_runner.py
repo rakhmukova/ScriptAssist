5509bd1928b5d6a8428a4f77675afe2646a09ea0
rakhmukova <lessness19@gmail.com>
2023-03-26 19:51:22 +0300

    Replace subprocess with QProcess

--- src/models/script_runner.py
+++ src/models/script_runner.py
@@ -1,13 +1,10 @@
-import select
-from subprocess import Popen, PIPE
-
-from PyQt6.QtCore import QObject, pyqtSignal
+from PyQt6.QtCore import QObject, pyqtSignal, QProcess
 
 
 class ScriptRunner(QObject):
     new_output = pyqtSignal(str)
     new_error = pyqtSignal(str)
-    process_finished = pyqtSignal(int)
+    finished = pyqtSignal(int)
 
     def __init__(self, interpreter_path, interpreter_options, script_path, parameters=None):
         super().__init__()
@@ -17,35 +14,25 @@ class ScriptRunner(QObject):
         if parameters is None:
             parameters = []
         self.parameters = list(map(str, parameters))
-        self.process = None
 
-    def start_process(self):
-        self.process = Popen([self.interpreter_path] + self.interpreter_options + [self.script_path] + self.parameters,
-                             stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-    def handle_stdout_and_stderr(self):
-        stdout = self.process.stdout
-        stderr = self.process.stderr
-
-        to_break = False
-        while not to_break:
-            ready, _, _ = select.select([stdout, stderr], [], [], 0.1)
-            if ready:
-                for stream in ready:
-                    if stream == stdout:
-                        output = stdout.readline()
-                        if output.decode() == '':
-                            to_break = True
-                        self.new_output.emit(output.strip().decode())
-                    elif stream == stderr:
-                        error = stderr.readline()
-                        self.new_error.emit(error.strip().decode())
-
-        stdout.close()
-        stderr.close()
-        exit_code = self.process.wait()
-        self.process_finished.emit(exit_code)
+        self.process = QProcess()
+        self.process.readyReadStandardOutput.connect(self.handle_stdout)
+        self.process.readyReadStandardError.connect(self.handle_stderr)
+        self.process.finished.connect(self.process_finished)
 
     def run(self):
-        self.start_process()
-        self.handle_stdout_and_stderr()
+        self.process.start(self.interpreter_path,
+                           self.interpreter_options + [self.script_path] + self.parameters)
+
+    def handle_stderr(self):
+        data = self.process.readAllStandardError()
+        stderr = bytes(data).decode()
+        self.new_error.emit(stderr)
+
+    def handle_stdout(self):
+        data = self.process.readAllStandardOutput()
+        stdout = bytes(data).decode()
+        self.new_output.emit(stdout)
+
+    def process_finished(self, exit_code):
+        self.finished.emit(exit_code)
