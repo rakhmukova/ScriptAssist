3ba871cb3842d8c459b28e6a41e3b7c5ffca1f20
rakhmukova <lessness19@gmail.com>
2023-03-27 01:01:54 +0300

    Document and refactor EditorPane

--- src/ui/widgets/editor_pane.py
+++ src/ui/widgets/editor_pane.py
@@ -1,34 +1,51 @@
 from PyQt6.QtGui import QColor
-from PyQt6.QtWidgets import QPlainTextEdit
+from PyQt6.QtWidgets import QPlainTextEdit, QWidget
 
 from src.models.keyword_chooser import KeywordChooser
+from src.models.script_config import ScriptConfig
 from src.ui.widgets.keyword_highlighter import KeywordHighlighter
 
 
 class EditorPane(QPlainTextEdit):
-    def __init__(self, parent=None):
+    """
+    A custom QPlainTextEdit widget for editing scripts.
+    """
+    def __init__(self, parent: QWidget = None):
+        """
+        Initializes an instance of the EditorPane.
+
+        :param parent: Parent widget, defaults to None.
+        """
         super().__init__(parent)
-        self.current_config = None
-        self.highlighter = None
-        self.keyword_color = QColor(0, 0, 255)  # blue color
+        self.__current_config = None
+        self.__highlighter = None
+        self.__keyword_color = QColor(0, 0, 255)  # blue color
         self.setObjectName('editorPane')
 
-    def set_script_config(self, script_config):
-        self.current_config = script_config
-        self.upload_from_config()
-        keywords = KeywordChooser.get_keywords(self.current_config.script_type)
-        self.highlighter = KeywordHighlighter(keywords, self.keyword_color, self.document())
+    def set_script_config(self, script_config: ScriptConfig):
+        """
+        Sets the current script configuration and updates the editor content.
 
-    def upload_from_config(self):
+        :param script_config: The script configuration object.
+        """
+        self.__current_config = script_config
+        self.__upload_from_config()
+        keywords = KeywordChooser.get_keywords(self.__current_config.script_type)
+        self.__highlighter = KeywordHighlighter(keywords, self.__keyword_color, self.document())
+
+    def __upload_from_config(self):
         self.clear()
-        file_path = self.current_config.path
+        file_path = self.__current_config.path
         with open(file_path, 'r') as f:
             script_content = f.read()
 
         self.appendPlainText(script_content)
 
     def save_script(self):
-        file_path = self.current_config.path
+        """
+        Saves the editor content to the current script file.
+        """
+        file_path = self.__current_config.path
         script_content = self.toPlainText()
         with open(file_path, 'w') as f:
             f.write(script_content)
