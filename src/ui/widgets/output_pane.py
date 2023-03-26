a17e2e60df5dcf419461a463e9a7370dc7267b1a
rakhmukova <lessness19@gmail.com>
2023-03-27 00:25:56 +0300

    Refactor and document OutputPane

--- src/ui/widgets/output_pane.py
+++ src/ui/widgets/output_pane.py
@@ -1,49 +1,70 @@
 from PyQt6.QtCore import pyqtSignal
-from PyQt6.QtWidgets import QTextEdit
+from PyQt6.QtWidgets import QTextEdit, QWidget
 
 from src.models.runner_factory import RunnerFactory
+from src.models.script_config import ScriptConfig
 
 
 class OutputPane(QTextEdit):
+    """
+    A QTextEdit widget that displays output from running a script.
+    """
     script_finished = pyqtSignal(int)
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.script_runner = None
-        self.current_config = None
-        self.setObjectName('outputPane')
+    def __init__(self, parent: QWidget = None):
+        """
+        Creates a new OutputPane instance.
 
-        self.output_cursor = self.textCursor()
+        :param parent: The parent widget of the OutputPane.
+        """
+        super().__init__(parent)
+        self.__script_runner = None
+        self.__current_config = None
+        self.setObjectName('outputPane')
         self.setReadOnly(True)
 
-    def set_script_config(self, script_config):
-        self.current_config = script_config
+    def set_script_config(self, script_config: ScriptConfig):
+        """
+        Sets the configuration for the script to be run.
+
+        :param script_config: The script configuration to set.
+        """
+        self.__current_config = script_config
         self.clear()
 
     def run_script(self):
-        if self.script_runner is None:
+        """
+        Runs the script using the configuration set by set_script_config().
+        """
+        if self.__script_runner is None:
             self.clear()
-            self.script_runner = RunnerFactory.get_runner(self.current_config)
-            self.script_runner.stderr_received.connect(self.handle_error)
-            self.script_runner.stdout_received.connect(self.handle_output)
-            self.script_runner.finished.connect(self.handle_finish)
-            self.script_runner.run()
+            self.__script_runner = RunnerFactory.get_runner(self.__current_config)
+            self.__script_runner.stderr_received.connect(self.__handle_error)
+            self.__script_runner.stdout_received.connect(self.__handle_output)
+            self.__script_runner.finished.connect(self.__handle_finish)
+            self.__script_runner.run()
 
     def stop_script(self):
-        if self.script_runner is not None:
-            self.script_runner.cancel()
-            self.script_runner = None
+        """
+        Stops the currently running script.
+        """
+        if self.__script_runner is not None:
+            self.__script_runner.cancel()
+            self.__script_runner = None
 
     def rerun_script(self):
+        """
+        Stops the currently running script and then runs it again.
+        """
         self.stop_script()
         self.run_script()
 
-    def handle_error(self, error):
+    def __handle_error(self, error: str):
         self.append(error)
 
-    def handle_output(self, output):
+    def __handle_output(self, output: str):
         self.append(output)
 
-    def handle_finish(self, exit_code):
-        self.script_runner = None
+    def __handle_finish(self, exit_code: int):
+        self.__script_runner = None
         self.script_finished.emit(exit_code)
