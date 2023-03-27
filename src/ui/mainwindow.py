18370351c64f56451deeed70db509e0ff0a07921
rakhmukova <lessness19@gmail.com>
2023-03-27 23:54:34 +0300

    Make fields private where necessary in MainWindow

--- src/ui/mainwindow.py
+++ src/ui/mainwindow.py
@@ -35,30 +35,30 @@ class MainWindow(QMainWindow):
         layout.setSpacing(0)
         layout.setContentsMargins(0, 0, 0, 0)
 
-        self.top_panel = TopPanel()
-        self.top_panel.run_button.clicked.connect(self.__run_script)
-        self.top_panel.stop_button.clicked.connect(self.__stop_script)
-        self.top_panel.edit_config_button.clicked.connect(self.__edit_script_config)
+        self.__top_panel = TopPanel()
+        self.__top_panel.run_button.clicked.connect(self.__run_script)
+        self.__top_panel.stop_button.clicked.connect(self.__stop_script)
+        self.__top_panel.edit_config_button.clicked.connect(self.__edit_script_config)
 
-        self.editor_pane = EditorPane()
+        self.__editor_pane = EditorPane()
 
-        self.line_number_area = LineNumberArea(self.editor_pane)
-        self.line_number_area.setFixedWidth(50)
-        self.editor_pane.blockCountChanged.connect(self.line_number_area.update_line_numbers)
-        self.editor_pane.verticalScrollBar().valueChanged.connect(self.line_number_area.update_scrollbar)
+        self.__line_number_area = LineNumberArea(self.__editor_pane)
+        self.__line_number_area.setFixedWidth(50)
+        self.__editor_pane.blockCountChanged.connect(self.__line_number_area.update_line_numbers)
+        self.__editor_pane.verticalScrollBar().valueChanged.connect(self.__line_number_area.update_scrollbar)
 
-        self.script_status_label = ScriptStatusLabel()
-        self.script_status_label.setObjectName('scriptStatusLabel')
-        self.script_status_label.setFixedHeight(25)
+        self.__script_status_label = ScriptStatusLabel()
+        self.__script_status_label.setObjectName('scriptStatusLabel')
+        self.__script_status_label.setFixedHeight(25)
 
-        self.output_pane = OutputPane()
-        self.output_pane.script_finished.connect(self.script_status_label.show_finish_status)
+        self.__output_pane = OutputPane()
+        self.__output_pane.script_finished.connect(self.__script_status_label.show_finish_status)
 
         self.__create_layout(layout)
 
-        self.run_action = None
-        self.stop_action = None
-        self.edit_config_action = None
+        self.__run_action = None
+        self.__stop_action = None
+        self.__edit_config_action = None
         self.__create_actions()
 
         self.__subscribe_to_config_change()
@@ -68,36 +68,36 @@ class MainWindow(QMainWindow):
         editor_layout = QHBoxLayout()
         editor_layout.setSpacing(0)
         editor_layout.setContentsMargins(0, 0, 0, 0)
-        editor_layout.addWidget(self.line_number_area)
-        editor_layout.addWidget(self.editor_pane)
+        editor_layout.addWidget(self.__line_number_area)
+        editor_layout.addWidget(self.__editor_pane)
 
-        layout.addWidget(self.top_panel)
+        layout.addWidget(self.__top_panel)
         layout.addLayout(editor_layout, stretch=2)
-        layout.addWidget(self.script_status_label)
-        layout.addWidget(self.output_pane, stretch=1)
+        layout.addWidget(self.__script_status_label)
+        layout.addWidget(self.__output_pane, stretch=1)
 
     def __create_actions(self):
-        self.run_action = QAction('Run', self)
-        self.run_action.setShortcut('Ctrl+R')
-        self.run_action.triggered.connect(self.__run_script)
-        self.addAction(self.run_action)
+        self.__run_action = QAction('Run', self)
+        self.__run_action.setShortcut('Ctrl+R')
+        self.__run_action.triggered.connect(self.__run_script)
+        self.addAction(self.__run_action)
 
-        self.stop_action = QAction('Stop', self)
-        self.stop_action.setShortcut('Ctrl+T')
-        self.stop_action.triggered.connect(self.__stop_script)
-        self.addAction(self.stop_action)
+        self.__stop_action = QAction('Stop', self)
+        self.__stop_action.setShortcut('Ctrl+T')
+        self.__stop_action.triggered.connect(self.__stop_script)
+        self.addAction(self.__stop_action)
 
-        self.edit_config_action = QAction('Edit configuration', self)
-        self.edit_config_action.setShortcut('Ctrl+E')
-        self.edit_config_action.triggered.connect(self.__edit_script_config)
-        self.addAction(self.edit_config_action)
+        self.__edit_config_action = QAction('Edit configuration', self)
+        self.__edit_config_action.setShortcut('Ctrl+E')
+        self.__edit_config_action.triggered.connect(self.__edit_script_config)
+        self.addAction(self.__edit_config_action)
 
     def __subscribe_to_config_change(self):
         signaled = self.config_changed
-        signaled.connect(self.editor_pane.on_config_changed)
-        signaled.connect(self.output_pane.on_config_changed)
-        signaled.connect(self.top_panel.on_config_changed)
-        signaled.connect(self.script_status_label.show_ready_status)
+        signaled.connect(self.__editor_pane.on_config_changed)
+        signaled.connect(self.__output_pane.on_config_changed)
+        signaled.connect(self.__top_panel.on_config_changed)
+        signaled.connect(self.__script_status_label.show_ready_status)
 
     @property
     def script_config(self):
@@ -115,9 +115,9 @@ class MainWindow(QMainWindow):
             self.script_config = config
 
     def __run_script(self):
-        self.editor_pane.save_script()
-        self.output_pane.run_script()
-        self.script_status_label.show_run_status()
+        self.__editor_pane.save_script()
+        self.__output_pane.run_script()
+        self.__script_status_label.show_run_status()
 
     def __stop_script(self):
-        self.output_pane.stop_script()
+        self.__output_pane.stop_script()
