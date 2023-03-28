ce47284c539879e881bf322b1459e8db39081c9c
rakhmukova <lessness19@gmail.com>
2023-03-28 22:17:58 +0300

    Use ScriptType enum over strings

--- src/models/keyword_chooser.py
+++ src/models/keyword_chooser.py
@@ -1,20 +1,23 @@
+from models.script_type import ScriptType
+
+
 class KeywordChooser:
     """
     A class that provides a list of commonly used keywords for a given script type.
     """
     @staticmethod
-    def get_keywords(script_type: str) -> list:
+    def get_keywords(script_type: ScriptType) -> list:
         """
         Get a list of commonly used keywords for the given script type.
 
         :param script_type: A string representing the script type (either 'Swift' or 'Kotlin').
         :return: A list of commonly used keywords for the given script type.
         """
-        if script_type == 'Swift':
+        if script_type == ScriptType.SWIFT:
             return ['let', 'var', 'if', 'in', 'else', 'switch', 'case', 'default',
                     'for', 'while', 'continue', 'break', 'return', 'func', 
                     'class', 'struct', 'enum', 'extension', 'protocol', 'init', 'deinit']
-        elif script_type == 'Kotlin':
+        elif script_type == ScriptType.UNDEFINED:
             return ['package', 'import', 'class', 'interface', 'fun', 'val',
                     'var', 'if', 'else', 'when', 'is', 'in', 'for', 'while',
                     'do', 'return', 'break', 'continue', 'throw', 'try', 'catch', 'finally']
