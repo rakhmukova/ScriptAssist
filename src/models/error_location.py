5452950121ec52ad2049260a67abeebd698e781b
rakhmukova <lessness19@gmail.com>
2023-04-06 00:03:24 +0300

    Add methods to convert ErrorLocation to str and viceversa

--- src/models/error_location.py
+++ src/models/error_location.py
@@ -34,3 +34,29 @@ class ErrorLocation:
         :return: The column number where the error occurred.
         """
         return self.__column_number
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the ErrorLocation object.
+
+        :return: A string representation of the ErrorLocation object.
+        """
+        return f'{self.__file_path}:{self.__line_number}:{self.__column_number}'
+
+    @classmethod
+    def from_string(cls, string: str) -> 'ErrorLocation':
+        """
+        Create a new ErrorLocation object from a string.
+
+        :param string: The string representation of the ErrorLocation object.
+        :return: A new ErrorLocation object.
+        :raises ValueError: If the string has an invalid format.
+        """
+        parts = string.split(':')
+        if len(parts) != 3:
+            raise ValueError(f'Invalid ErrorLocation string: {string}')
+
+        file_path, line_number, column_number = parts
+        line_number = int(line_number)
+        column_number = int(column_number)
+        return cls(file_path, line_number, column_number)
